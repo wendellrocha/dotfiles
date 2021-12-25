fb1f344ff22e1091bfa38c5d8c419e03be0c8b95
Wendell Rocha <wendellrocha@outlook.com>
2021-12-25 19:58:12 -0300

    windows powershell dotfiles

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,33 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+Import-Module Terminal-Icons
+Import-Module PSFzf
+Import-Module posh-git
+
+function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'jandedobbeleer.omp.json'
+
+Set-PSReadLineOption -PredictionSource History
+
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+$Env:BAT_CONFIG_PATH = Join-Path (Get-ScriptDirectory) 'bat.conf'
+
+Set-Alias grep findstr
+Set-Alias cat bat
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
+# Utilities
+function which ($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
