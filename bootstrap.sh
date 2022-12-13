a0826cd289138cd23eec796fea00b0e9f874b56d
Wendell Rocha <wendell.rocha@mblabs.com.br>
2022-12-13 08:33:22 -0300

    feat: addding prezto install

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-if [ ! -x "$(command -v starship)" ]; then
-    curl -sS https://starship.rs/install.sh | sh
+if [ ! -d "$HOME/.zprezto" ]; then
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+    
+    setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+        ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
 fi
 
 if [ ! -d "$HOME/.zprezto-prompt-contrib" ]; then
     git clone --recurse-submodules https://github.com/mattmc3/prezto-prompt-contrib ${ZDOTDIR:-$HOME}/.zprezto-prompt-contrib
+fi
+
+if [ ! -x "$(command -v starship)" ]; then
+    curl -sS https://starship.rs/install.sh | sh
 fi
 
 cat > ~/.zshrc << EOF
